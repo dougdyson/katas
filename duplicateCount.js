cd4c8ac6151e446f5052631669529628ab1c8fc0
Unfamous Dyson <hatheadninja@gmail.com>
2020-11-12 12:39:52 -0800

    add approaches to look at later

--- duplicateCount.js
+++ duplicateCount.js
@@ -12,3 +12,15 @@ function duplicateCount(text) {
 }
 
 module.exports = duplicateCount;
+
+// to dig into further...
+//
+// function duplicateCount(text){
+//   return text.toLowerCase().split('').filter(function(val, i, arr){
+//     return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+//   }).length;
+// }
+//
+// function duplicateCount(text){
+//   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+// }
