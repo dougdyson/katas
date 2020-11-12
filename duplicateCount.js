a3a67bfc3c1330f738d096d019ba031ecf9f5828
Unfamous Dyson <hatheadninja@gmail.com>
2020-11-12 00:36:35 -0800

    complete 1-4

---
+++ duplicateCount.js
@@ -0,0 +1,29 @@
+function duplicateCount(string) {
+
+  const test = 'aabBcde';
+  
+  const str = string.toLowerCase();
+  let dupeCount = 0;
+  const stringLength = str.length;
+
+  if (!str) {
+    return dupeCount;
+  }
+
+  for (let i = 0; i < stringLength; i++) {
+
+    const currentString = str.slice(i);
+    const currentChar = currentString.slice(0,1);
+    const firstIndex = currentString.indexOf(currentChar)
+    const lastIndex = currentString.lastIndexOf(currentChar)
+    
+    if (firstIndex < lastIndex) {
+      dupeCount++;
+      console.log(dupeCount);
+    }
+  }
+  console.log('dupeCount:', dupeCount);
+  return dupeCount;
+}
+
+module.exports = duplicateCount;
---
+++ duplicateCount.test.js
@@ -0,0 +1,29 @@
+// From:
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
+
+
+const duplicateCount = require('./duplicateCount');
+
+test('checks for duplicate instances of characters within an empty string', () => {
+  expect(duplicateCount("")).toBe(0);
+});
+
+test('checks for duplicate instances of characters within a string with no duplicates', () => {
+  expect(duplicateCount("abcde")).toBe(0);
+});
+
+test('checks for duplicate instances of characters within a string with duplicates', () => {
+  expect(duplicateCount("aabbcde")).toBe(2);
+});
+
+test('checks for duplicate instances of characters within a string with duplicates of mixed case', () => {
+  expect(duplicateCount("aabBcde")).toBe(2);
+});
+
+test('checks for duplicate instances of characters within a string with duplicates which are not adjacent', () => {
+  expect(duplicateCount("Indivisibility")).toBe(1);
+});
+
+test.skip('checks for duplicate instances of characters within a string with duplicates which are not adjacent', () => {
+  expect(duplicateCount("Indivisibilities")).toBe(2);
+});
