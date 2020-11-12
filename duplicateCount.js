5432675a0632a4049dfba25ac13ebd06346d7555
Unfamous Dyson <hatheadninja@gmail.com>
2020-11-12 12:14:56 -0800

    refactor out function

--- duplicateCount.js
+++ duplicateCount.js
@@ -1,23 +1,12 @@
-function duplicateCount(string) {
+function duplicateCount(text) {
   
-  const str = string.toLowerCase().split('').sort();
+  const str = text.toLowerCase().split('').sort();
   let dupeChars = [];
-
-  function checkIfCharExists(character) {
-    for (let i = 0; i < dupeChars.length; i++) {
-      if (dupeChars[i] === character) {
-        return true;
-      }
-    }
-    return false;
-  }
 
   for (let i = 0; i < str.length; i++) {
     const currentChar = str[i];
-    if (currentChar === str[i + 1]) {
-      if (!checkIfCharExists(currentChar)) {
-        dupeChars.push(currentChar)
-      }
+    if ((currentChar === str[i + 1]) && !dupeChars.includes(currentChar)) {
+      dupeChars.push(currentChar)
     }
   }
   return dupeChars.length;
