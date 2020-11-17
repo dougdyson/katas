9d3798d3f021699b06804a57424b343595bddbcb
Unfamous Dyson <dougdyson@gmail.com>
2020-11-16 16:31:04 -0800

    include kata source in comment

--- challenges/descendingOrder/descendingOrder.test.js
+++ challenges/descendingOrder/descendingOrder.test.js
@@ -1,3 +1,6 @@
+// from:
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155/javascript
+
 const descendingOrder = require('./descendingOrder');
 
 test('sorts non-integer integers into highest possible number', () => {
