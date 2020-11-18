2587929b09b3480748f211ada7b92891caeb3764
Unfamous Dyson <dougdyson@gmail.com>
2020-11-17 23:36:44 -0800

    add tests

---
+++ challenges/persistence/persistence.test.js
@@ -0,0 +1,8 @@
+const persistence = require('./persistence');
+
+test('Initial Tests', () => {
+  expect(persistence(39)).toBe(3);
+  expect(persistence(4)).toBe(0);
+  expect(persistence(25)).toBe(2);
+  expect(persistence(999)).toBe(4);
+});
