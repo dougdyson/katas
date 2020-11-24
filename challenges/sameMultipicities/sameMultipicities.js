e04725b695584c7789ce21174730154cf090d1de
Unfamous Dyson <dougdyson@gmail.com>
2020-11-24 00:09:20 -0800

    add passing and failing test

--- challenges/sameMultipicities/sameMultipicities.js
+++ challenges/sameMultipicities/sameMultipicities.js
@@ -2,6 +2,10 @@ function sameMultiplicity (arr1, arr2) {
 
   let isSame = false;
 
+  if (arr1.length > 0 && (arr1.length == arr2.length)){
+    isSame = true;
+  }
+
   return isSame;
 }
 
--- challenges/sameMultipicities/sameMultipicities.test.js
+++ challenges/sameMultipicities/sameMultipicities.test.js
@@ -9,6 +9,12 @@ test('returns false if empty array passed as argument', () => {
 
 a1 = [121, 144, 19, 161, 19, 144, 19, 11];
 a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
-test('checks whether the two arrays have the "same" elements, with the same multiplicities, regardless of the order.', () => { 
+test('returns true when the two arrays have the "same" elements, with the same multiplicities, regardless of the order.', () => { 
   expect(sameMultiplicity(a1, a2)).toBe(true); 
+})
+
+a1 = [121, 144, 19, 161, 19, 144, 19, 12];
+a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+test('returns false when the two arrays have the different elements, with the same multiplicities, regardless of the order.', () => { 
+  expect(sameMultiplicity(a1, a2)).toBe(false); 
 })
