3dfe8f5737cf939747deadb1e2639c3172daa91d
Unfamous Dyson <dougdyson@gmail.com>
2020-11-23 23:59:26 -0800

    add test

--- challenges/sameMultipicities/sameMultipicities.test.js
+++ challenges/sameMultipicities/sameMultipicities.test.js
@@ -3,6 +3,10 @@
 
 const sameMultiplicity = require ('./sameMultipicities');
 
+test('returns false if empty array passed as argument', () => { 
+  expect(sameMultiplicity([], [])).toBe(false); 
+})
+
 a1 = [121, 144, 19, 161, 19, 144, 19, 11];
 a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
 test('checks whether the two arrays have the "same" elements, with the same multiplicities, regardless of the order.', () => { 
