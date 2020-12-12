76f418522b6dd32967edc47765b45fbec265acfd
Unfamous Dyson <dougdyson@gmail.com>
2020-12-11 23:57:18 -0800

    add valid roman numerals

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,6 +1,10 @@
 function romanNumeralDecoder (romanNumeral) {
 
   if (typeof(romanNumeral) != String) return null;
+
+  const validRomanNumerals = [I, II, III, IV, V, VI, VII, VIII, IX, X, XI, XII, XIII, XIV, XV, XVI, XVII, XVIII, XIX, XX, L, C, D, M];
+
+  
   
   return romanNumeral;
 
