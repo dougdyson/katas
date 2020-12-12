ea15ad432f7cc7369522157c3bf135216c1badb2
Unfamous Dyson <dougdyson@gmail.com>
2020-12-12 00:11:29 -0800

    fix input type validation bug

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,10 +1,13 @@
-function romanNumeralDecoder (romanNumeral) {
+tfunction romanNumeralDecoder (romanNumeral) {
 
-  if (typeof(romanNumeral) != String) return null;
-
-  const validRomanNumerals = [I, II, III, IV, V, VI, VII, VIII, IX, X, XI, XII, XIII, XIV, XV, XVI, XVII, XVIII, XIX, XX, L, C, D, M];
-
+ype  if (typeof(romanNumeral) !== 'string' || romanNumeral === '') return null;
   
+  const validRomanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'L', 'C', 'D', 'M'];
+
+  const length = romanNumeral.length;
+  const romanNumeralArr = romanNumeral.split();
+
+
   
   return romanNumeral;
 
