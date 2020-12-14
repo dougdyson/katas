885bf736134b8bb717a543dffc82c3612cee4a37
Unfamous Dyson <dougdyson@gmail.com>
2020-12-14 00:15:11 -0800

    add cumulative addition

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -3,20 +3,25 @@ function romanNumeralDecoder (romanNumeral) {
   if (typeof(romanNumeral) !== 'string' || romanNumeral === '') return null;
   
   const validRomanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'L', 'C', 'D', 'M'];
-  const romanNumerals = [{'I':1}, {'V':5}, {'X':10}, {'L':50}, {'C':50}, {'D':500}, {'M':1000}];
-
-  
+  const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':50, 'D':500, 'M':1000};
 
   const length = romanNumeral.length;
-  const romanNumeralArr = romanNumeral.split('');
+  let decodedNumeral = null;
 
-  // test for non-valid strings
-  if (!validRomanNumerals.includes(romanNumeral)) {
-    return null;
+  for (let i = 0; i < length; i++){
+
+    // get each char
+    const currentLetter = romanNumeral.charAt(i) //?
+    decodedNumeral = decodedNumeral + romanNumerals[currentLetter]; //?
+
   }
   
-  return romanNumeral;
+  return decodedNumeral;
+;
 
 }
+
+console.log(romanNumeralDecoder('XXI')); 
+
 
 module.exports = romanNumeralDecoder;
