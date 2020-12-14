78fef5c5a73d4d7e0f3854051d5305c527d0eba9
Unfamous Dyson <dougdyson@gmail.com>
2020-12-13 23:48:12 -0800

    add roman numeral mapping array object

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -3,18 +3,17 @@ function romanNumeralDecoder (romanNumeral) {
   if (typeof(romanNumeral) !== 'string' || romanNumeral === '') return null;
   
   const validRomanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'L', 'C', 'D', 'M'];
+  const romanNumerals = [{'I':1}, {'V':5}, {'X':10}, {'L':50}, {'C':50}, {'D':500}, {'M':1000}];
+
+  
 
   const length = romanNumeral.length;
-  const romanNumeralArr = romanNumeral.split();
-
-  console.log(romanNumeralArr);
+  const romanNumeralArr = romanNumeral.split('');
 
   // test for non-valid strings
   if (!validRomanNumerals.includes(romanNumeral)) {
     return null;
   }
-
-
   
   return romanNumeral;
 
