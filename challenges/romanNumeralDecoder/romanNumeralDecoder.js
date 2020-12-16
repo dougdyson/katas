a68646fea8923195e85aacd3710013ab6b84408b
Unfamous Dyson <dougdyson@gmail.com>
2020-12-15 16:48:22 -0800

    correct mapping object value

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,6 +1,6 @@
 function romanNumeralDecoder (romanNumeral) {
 
-  const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':50, 'D':500, 'M':1000};
+  const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
 
   let decodedNumeral = null;
   let accumulator = null;
