2b429c9cb087d83b369d8a2eeb19754be38e7736
Unfamous Dyson <dougdyson@gmail.com>
2020-12-21 16:10:40 -0800

    clean up code

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,17 +1,17 @@
 function romanNumeralDecoder (romanNumeral) {
 
+  if (romanNumeral === null) return null;
+  
   const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
-
+  
   let decodedNumeral = null;
   let accumulator = null;
   let prevValue = null;
-
-  if (romanNumeral === null) return null;
   
   for (let i = 0; i < romanNumeral.length; i++){
     
     const currentLetter = romanNumeral.charAt(i); 
-    const currentValue = romanNumerals[currentLetter];
+    const currentValue = romanNumerals[currentLetter]; 
     
     if (romanNumerals[currentLetter] === undefined) {
       return null;
@@ -21,7 +21,7 @@ function romanNumeralDecoder (romanNumeral) {
       accumulator += currentValue;
       decodedNumeral+= currentValue;
     } else {
-      accumulator = currentValue - (prevValue * 2);
+      accumulator = currentValue - (prevValue * 2); 
       decodedNumeral += accumulator;
     }
     prevValue = currentValue;
@@ -30,14 +30,14 @@ function romanNumeralDecoder (romanNumeral) {
   return decodedNumeral; 
 }
 
-// romanNumeralDecoder('I'); //? 
-// romanNumeralDecoder('II'); //? 
-// romanNumeralDecoder('IV'); //?
-// romanNumeralDecoder('XXI'); //? 
-// romanNumeralDecoder('XLIV'); //?
-// romanNumeralDecoder('MMV'); //?
-// romanNumeralDecoder('LXXXVIII'); //?
-//romanNumeralDecoder('MDCLXIV');  //?
+// romanNumeralDecoder('I');  
+// romanNumeralDecoder('II');  
+// romanNumeralDecoder('IV'); 
+// romanNumeralDecoder('XXI');  
+// romanNumeralDecoder('XLIV'); 
+// romanNumeralDecoder('MMV'); 
+// romanNumeralDecoder('LXXXVIII'); 
+// romanNumeralDecoder('MDCLXIV');  
 
 
 module.exports = romanNumeralDecoder;
