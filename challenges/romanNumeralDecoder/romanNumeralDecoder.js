eba7dc32d36fc164983404c4d37c2b04c445a237
Unfamous Dyson <dougdyson@gmail.com>
2020-12-23 23:48:31 -0800

    prep for refactor

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,5 +1,6 @@
 function romanNumeralDecoder (romanNumeral) {
 
+  // refactor this function to use an array and reduce
   if (romanNumeral === null) return null;
   
   const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
