a7e15bac20e63c82a828621d18239e42e28df277
Unfamous Dyson <dougdyson@gmail.com>
2020-12-23 23:57:14 -0800

    comment reduce syntax

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,6 +1,8 @@
 function romanNumeralDecoder (romanNumeral) {
-
+  
   // refactor this function to use an array and reduce
+  // reduce syntax: array.reduce((accumulator, input) => {filter logic})
+
   if (romanNumeral === null) return null;
   
   const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
