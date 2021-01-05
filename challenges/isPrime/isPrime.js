cb0d5e0f459ece36b590db153e4f66bccdeac7f1
Unfamous Dyson <dougdyson@gmail.com>
2021-01-05 01:27:30 -0800

    add precalculated primes & refactor

--- challenges/isPrime/isPrime.js
+++ challenges/isPrime/isPrime.js
@@ -1,12 +1,15 @@
+const primeNumbers = require('./primeNumbers');
+const primes = primeNumbers();
+
 function isPrime(number) {
 
   // rudimentary input validation
-  if (typeof(number) !== 'number' || number === null || number < 2) {
-    return false;
-  }
+  if (typeof(number) !== 'number' || number === null || number < 2) return false;
 
-  // the below works and passes the sample tests but it times out in the codewar test runner.
-  // codewars' generic recommendation is refactoring for effeciency.
+  // check precalculated primes
+  if (primes.includes(number)) return true;
+
+  // if input argument not in precalculated primes, test via modulo
   for (let i = 2; i < number; i++) {
     if (number % i === 0) {
       return false;
@@ -15,7 +18,7 @@ function isPrime(number) {
 
   return true;
 
-  // recursion or reduce seem like potential approaches.
+  // recursion or reduce are perhaps potential refactoring approaches.
   // the trick is reducing the amount of computation required as much as possible, but
   // calculating whether a modulo divisor is necessary perhaps costs as much as
   // calculating the modulo operation result.
