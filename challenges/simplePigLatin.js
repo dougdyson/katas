361213384817cbe341de5632512e42a45df489f6
Unfamous Dyson <dougdyson@gmail.com>
2021-01-21 23:42:03 -0800

    initialize variables

--- challenges/simplePigLatin.js
+++ challenges/simplePigLatin.js
@@ -1,10 +1,12 @@
 function pigIt(englishSentence) {
   
-  const sentence = englishSentence.split(' ')
+  let pigSentence = '';
 
-  for (let i = 0; i < sentence.length; i++) {
-    const element = sentence[i];
-    console.log(element);
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  for (let i = 0; i < englishSentence.length; i++) {
+    const letter = englishSentence[i];
+    console.log(letter);
   }
   
   return null;
