9e89dd0a444f3e35df69d29858338c8d6f22ba5a
Unfamous Dyson <dougdyson@gmail.com>
2021-01-22 19:54:31 -0800

    refactor to calculate rihght

--- challenges/romanNumeralDecoder/romanNumeralDecoder.js
+++ challenges/romanNumeralDecoder/romanNumeralDecoder.js
@@ -1,36 +1,37 @@
-function romanNumeralDecoderV2 (romanNumeral) {
+function romanNumeralDecoder (romanNumeral) {
   
-  // refactor this function to use an array and reduce
-  // reduce syntax: array.reduce((accumulator, currentValue, currentIndex, array), initialValue)
+  // refactor this function to calculate from right to left, rather than left to right
+  // this direction avoids having to recalcuate for edge cases
 
-  if (romanNumeral === null || romanNumeral === '' || typeof(romanNumeral) !== 'string') return null;
-
-  const encodedNumerals = romanNumeral.split('');
+  if (romanNumeral === null) return null;
   
   const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
-  
-  //let decodedNumeral = null;
-  let accumulator = null;
-  //let prevValue = null;
 
-  return encodedNumerals.reduce((decodedNumeral, prevValue) =>{
-    const currentLetter = encodedNumerals[i]; 
-    const currentValue = romanNumerals[currentLetter]; 
+  let decodedNumeral = null;
+  let prevValue = null;
+
+  for (let i = romanNumeral.length - 1; i >= 0; i--) {
+    
+    const currentLetter = romanNumeral.charAt(i); 
+    const currentValue = romanNumerals[currentLetter];
+    console.log('currentLetter:', currentLetter, '; currentValue:', currentValue, '; prevValue:', prevValue, '; decodedNumeral:', decodedNumeral);
     
     if (romanNumerals[currentLetter] === undefined) {
       return null;
     }
 
-    if (prevValue >= currentValue) { 
-      accumulator += currentValue;
-      decodedNumeral += currentValue;
+    if (prevValue > currentValue) {
+      console.log('edge case');
+      decodedNumeral = decodedNumeral + currentValue - prevValue;
     } else {
-      accumulator = currentValue - (prevValue * 2); 
-      decodedNumeral += accumulator;
+      console.log('simple addition');
+      decodedNumeral = decodedNumeral + currentValue;
     }
+    //console.log(decodedNumeral, prevValue, currentValue);
     prevValue = currentValue;
-  })
-  
+  }
+  console.log('EOF');
+  return decodedNumeral;
 
 }
 
@@ -43,7 +44,7 @@ function romanNumeralDecoderV2 (romanNumeral) {
 // is not as easy to follow as I would like.
 // Seeing that I needed an accumulator, going to do a refactor using an array with the reduce method.
 //
-function romanNumeralDecoder (romanNumeral) {
+function romanNumeralDecoderV1 (romanNumeral) {
 
   if (romanNumeral === null) return null;
   
