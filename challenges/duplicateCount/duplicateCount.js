9abe65c2f40068776d9d81838b8e28cab98fa10d
Unfamous Dyson <dougdyson@gmail.com>
2021-02-02 00:27:53 -0800

    refactor for conciseness, pass 3 of 5 tests

--- challenges/duplicateCount/duplicateCount.js
+++ challenges/duplicateCount/duplicateCount.js
@@ -1,46 +1,34 @@
 function duplicateCount(text) {
+  return [...text].sort().reduce((p,c,i,a) => (c === a[i+1] ? p+1 : p ), 0); //?
+}
+// UPDATE: refactored above with 65% reduction in code!
 
-  // here is a refactor using lastIndexOf()
-  // which removes the need to pre-sort the array.
+duplicateCount('aabbcde'); //?
 
-  const string = text.toLowerCase().split('');
-  let duplicates = [];
+
+  
+function duplicateCountV2(text) {
+  //first refactor, with includes
+  
+  const string = text.toLowerCase().split('').sort();
   for (let i = 0; i < string.length; i++) {
-    if (string.lastIndexOf(string[i]) !== i && !duplicates.includes(string[i])) {
+    if ((string[i] === string[i + 1]) && !duplicates.includes(string[i])) {
       duplicates.push(string[i]);
     }
   }
-  return duplicates.length;
+}
+// in earlier attempts, was trying to use indexOf() !== lastIndexOf()
+// but didn't find a way for it to account for instances where there
+// were 3+ instances of a character. 
+// 
+// the commented out example below this function using filter 
+// has the solution logic. can also see how filter encapsulates
+// the classic c-style for loop I used in my first refactor
+
+function duplicateCountV1(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
 }
 
-  duplicateCount('Hello Dolly'); //?
-
-  module.exports = duplicateCount;
-  
-  // to dig into further: filter && match
-  //
-  //const string = text.toLowerCase().split('').sort();
-
-  // first refactor, with includes
-  // for (let i = 0; i < string.length; i++) {
-  //   if ((string[i] === string[i + 1]) && !duplicates.includes(string[i])) {
-  //     duplicates.push(string[i]);
-  //   }
-  // }
-
-  // in earlier attempts, was trying to use indexOf() !== lastIndexOf()
-  // but didn't find a way for it to account for instances where there
-  // were 3+ instances of a character. 
-  // 
-  // the commented out example below this function using filter 
-  // has the solution logic. can also see how filter encapsulates
-  // the classic c-style for loop I used in my first refactor
-  // function duplicateCount(text){
-//   return text.toLowerCase().split('').filter(function(val, i, arr){
-//     return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
-//   }).length;
-// }
-//
-// function duplicateCount(text){
-//   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
-// }
+module.exports = duplicateCount;
