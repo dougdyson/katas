ab56708564f0f6d4725c8a7a3824762e17e6b002
Unfamous Dyson <dougdyson@gmail.com>
2021-02-02 14:38:16 -0800

    format code

--- challenges/descendingOrder/descendingOrder.js
+++ challenges/descendingOrder/descendingOrder.js
@@ -1,4 +1,4 @@
 function descendingOrder(num) {
-  return parseInt (num.toString().split("").sort((a, b) => (b-a)).join(''));
+  return parseInt (num.toString().split('').sort((a,b) => (b-a)).join(''));
 }
 module.exports = descendingOrder;
