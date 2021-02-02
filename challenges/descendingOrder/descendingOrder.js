4777d9425a23a5556f161461a6b5bea950dad1a2
Unfamous Dyson <dougdyson@gmail.com>
2021-02-01 23:47:13 -0800

    implement arrow function call

--- challenges/descendingOrder/descendingOrder.js
+++ challenges/descendingOrder/descendingOrder.js
@@ -1,5 +1,4 @@
 function descendingOrder(num) {
-  return parseInt (num.toString().split("").sort(function(a, b){return b-a}).join(''));
+  return parseInt (num.toString().split("").sort((a, b) => (b-a)).join(''));
 }
-
 module.exports = descendingOrder;
