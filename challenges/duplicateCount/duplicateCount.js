be6e80e28f6f1ddd112d9f3c1f89b36fd95a5f91
Unfamous Dyson <dougdyson@gmail.com>
2021-02-04 14:59:18 -0800

    cleanup code

--- challenges/duplicateCount/duplicateCount.js
+++ challenges/duplicateCount/duplicateCount.js
@@ -1,23 +1,18 @@
 function duplicateCount(text) {
-  return text.toLowerCase()
-             .split('')
-             .sort()
-             .reduce((p,c,i,a) => (c === a[i+1] && c != a[i-1] ? p+1 : p ), 0);
+  return text.toLowerCase().split('').sort().reduce((p,c,i,a) => (c === a[i+1] && c != a[i-1] ? p+1 : p ), 0);
 }
-// UPDATE: refactored above with 58% reduction in code!
+// UPDATE: refactored above with 59% reduction in code!
   
 function duplicateCountV1(text) {
   //first refactor, with includes
 
   const string = text.toLowerCase().split('').sort();
   let duplicates = [];
-  
   for (let i = 0; i < string.length; i++) {
     if ((string[i] === string[i + 1]) && !duplicates.includes(string[i])) {
       duplicates.push(string[i]);
     }
   }
-
   return duplicates.length;
 }
 // in earlier attempts, was trying to use indexOf() !== lastIndexOf()
