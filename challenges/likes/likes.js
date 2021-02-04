c1a7dc6cd3a63b21e3331c5bfd32dac16e49cbf2
Unfamous Dyson <dougdyson@gmail.com>
2021-02-04 14:55:26 -0800

    comment code

--- challenges/likes/likes.js
+++ challenges/likes/likes.js
@@ -1,19 +1,9 @@
 function likes(names) {
-
-  // first iteration
-  // ===============
-  // if (names.length) {
-  //   if (names.length === 1) return names[0] + ' likes this';
-  //   if (names.length === 2) return names[0] + ' and ' + names[1] + ' like this';
-  //   if (names.length === 3) return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
-  //   if (names.length >= 4) return names[0] + ', ' + names[1] + ' and ' + (names.length - 2)  + ' others like this';
-  // }
-  // return 'no one likes this';
-
+  
   // second iteration
   // ================
-  // use of switch can reduce amount of code and easier to read
-
+  // use of switch reduces 2% of code and easier to read
+  
   switch (names.length) {
     case 0:  return 'no one likes this'; break;
     case 1:  return names[0] + ' likes this'; break;
@@ -21,7 +11,18 @@ function likes(names) {
     case 3:  return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
     default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2)  + ' others like this'; break;
   }
-
 }
-
+1 - 400 / 410; //?
 module.exports = likes;
+
+// first iteration
+// ===============
+function likesV1(names){
+  if (names.length) {
+    if (names.length === 1) return names[0] + ' likes this';
+    if (names.length === 2) return names[0] + ' and ' + names[1] + ' like this';
+    if (names.length === 3) return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+    if (names.length >= 4) return names[0] + ', ' + names[1] + ' and ' + (names.length - 2)  + ' others like this';
+  }
+  return 'no one likes this';
+}
