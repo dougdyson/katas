abcb80ad4ffa0126ba28f03a28f5adf3d11c12b2
Unfamous Dyson <dougdyson@gmail.com>
2021-02-04 14:40:38 -0800

    refactor for recursion but v2 is the best

--- challenges/persistence/persistence.js
+++ challenges/persistence/persistence.js
@@ -1,18 +1,30 @@
 function persistence(num) {
   for (var i = 0; num > 9; i++) {
-    num = num.toString().split('').reduce((a,c) => a * c); //?
+    num = num.toString().split('').reduce((a,c) => a * c);
   }
-  return i; //?
+  return i;
 }
+// The V2 refactor above removes recursion and reduces code by 65%
+// UPDATE: after reviewing V3 refactor, going with this one!
 
-persistence(4); //?
+function persistenceV3(num) {
+  return (num > 9)
+    ? 1 + persistence(num.toString().split('').reduce((a,c) => a * c))
+    : 0
+}
+// The V3 refactor above uses recursion but reduces code by 68%.
+// From a risk perspective, feel it might be better to use the 
+// V2 refactor, which doesn't use recursion nor if statements 
+// and is only 3% larger. V2 also seems a bit easier to follow, 
+// declaring key variables in one place (for statement) instead of 
+// spreading them out in the logic.
 
 function persistenceV1(num) {
   let persistCounter = 0;
   if (num > 9) {
     function persist(numbers) {
-      const product = numbers.toString().split('').reduce((total, factor) => total * factor); //?
-      persistCounter++; //?
+      const product = numbers.toString().split('').reduce((total, factor) => total * factor);
+      persistCounter++;
       if (product > 9) {
         persist(product)
       }
@@ -21,34 +33,7 @@ function persistenceV1(num) {
   }
   return persistCounter;
 }
+// The above V1 of this code is horrible! Repeating some conditional logic, unnecessary conditional logic,
+// and too verbose. Props for chaining functions though!
+
 module.exports = persistence;
-
-
-// most popular, with map & reduce
-// =========================
-// function persistence(num) {
-//   var times = 0;
-//   num = num.toString();
-//   while (num.length > 1) {
-//     times++;
-//     num = num.split('').map(Number).reduce((a, b) => a * b).toString();
-//   }
-//   return times;
-// }
-
-// shorter with reduce..
-// =====================
-// function persistence(num) {
-//   for (var i = 0; num > 9; i++) {
-//     num = num.toString().split('').reduce((t, c) => c * t);
-//   }
-//   return i;
-// }
-
-// even shorter reduce with ternerary
-// ======================
-// const persistence = num => {
-//   return `${num}`.length > 1 
-//     ? 1 + persistence(`${num}`.split('').reduce((a, b) => a * +b)) 
-//     : 0;
-// }
