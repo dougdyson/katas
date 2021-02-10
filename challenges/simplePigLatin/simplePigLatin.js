65d2d33536001061728ab1c71f6d9d57c60b1e2d
Unfamous Dyson <dougdyson@gmail.com>
2021-02-09 23:59:20 -0800

    refactor

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -1,18 +1,18 @@
 function pigIt(englishSentence) {
   // consider ascii values
 
-  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const isLetter = (char) => char.toLowerCase().charCodeAt(0) > 96 && char.toLowerCase().charCodeAt(0) < 123 ? true : false; //?
 
   for (let i = 0; i < englishSentence.length; i++) {
     const letter = englishSentence[i];
-    letters.includes(letter)
-      ? console.log(letter, ' is a letter')
-      : console.log(letter, ' is not a letter')
-    }
+    isLetter(letter) //?
+  }
   
   
   return null;
 
 }
+
+pigIt("Hello World!");
 
 module.exports = pigIt;
