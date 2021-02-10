69167e1365bcbb2aa78a52aea1b7c697f439d2d9
Unfamous Dyson <dougdyson@gmail.com>
2021-02-10 00:06:36 -0800

    add pseudo code for next steps

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -1,12 +1,16 @@
 function pigIt(englishSentence) {
 
-  const isLetter = (char) => char.toLowerCase().charCodeAt(0) > 96 && char.toLowerCase().charCodeAt(0) < 123 ? true : false; //?
+  const isLetter = (char) => char.toLowerCase().charCodeAt() > 96 && char.toLowerCase().charCodeAt() < 123 ? true : false; //?
+  let pigSentence = '';
 
   for (let i = 0; i < englishSentence.length; i++) {
     const letter = englishSentence[i];
     isLetter(letter) //?
+    
+    // TO DO:
+    // - select words and transform
+    // - build new string
   }
-  
   
   return null;
 
