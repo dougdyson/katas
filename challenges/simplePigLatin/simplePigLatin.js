666b2c97758fa29b78a3ae1a05ffa7d260af7417
Unfamous Dyson <dougdyson@gmail.com>
2021-02-09 23:42:53 -0800

    refactor with ternerary

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -1,18 +1,15 @@
 function pigIt(englishSentence) {
   // consider ascii values
-  
-  let pigSentence = englishSentence;
 
   const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   for (let i = 0; i < englishSentence.length; i++) {
     const letter = englishSentence[i];
-    if (letters.includes(letter)) {
-      console.log(letter, ' is a letter');
-    } else {
-      console.log(letter, ' is not a letter');
+    letters.includes(letter)
+      ? console.log(letter, ' is a letter')
+      : console.log(letter, ' is not a letter')
     }
-  }
+  
   
   return null;
 
