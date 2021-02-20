14142355c7e3a41c25edfde541b0fba7ee68981d
Unfamous Dyson <dougdyson@gmail.com>
2021-02-20 01:25:04 -0800

    experiment with regex

--- regex-reference.js
+++ regex-reference.js
@@ -1,9 +1,11 @@
-let re1 = /ab+c/;
+let re1 = /ab+c/; //?
 
 let re2 = new RegExp('ab+c'); //?
 
 let myRe = /d(b+)d/g;
-let myArray = myRe.exec('cdbbdbsbz');
+let myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz');
+
+myArray[1]; //?
 
 
 
