9282e5077180b8fa678ccdc3fac0d93520f2216d
Unfamous Dyson <dougdyson@gmail.com>
2021-02-24 23:47:40 -0800

    experiment with regex

--- regex-reference.js
+++ regex-reference.js
@@ -3,9 +3,13 @@ let re1 = /ab+c/; //?
 let re2 = new RegExp('ab+c'); //?
 
 let myRe = /d(b+)d/g;
-let myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz');
+let myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz'); //?
 
 myArray[1]; //?
+
+// find space
+ let phrase = /l(o)/g;
+ let space = phrase.exec('Hello World!'); //?
 
 
 
