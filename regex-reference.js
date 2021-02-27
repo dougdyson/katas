c4b2f380869b17257e828a4df47ce2dd8464ce82
Unfamous Dyson <dougdyson@gmail.com>
2021-02-27 00:26:22 -0800

    clean up code

--- regex-reference.js
+++ regex-reference.js
@@ -1,19 +1,7 @@
-const re1 = /ab+c/; //?
-
-const re2 = new RegExp('ab+c'); //?
-
-const myRe = /d(b+)d/g;
-const myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz'); //?
-
-myArray[1]; //?
-
-// find space
-const phrase = / /g;
-const space = phrase.exec('Hello World!'); //?
-
-// find word
 const s = "Hello World!";
+// find space
 const r = / /g;
+// find word
 const i = r.exec(s).index; //?
 // respell word
 const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase() + 'ay'; //?
