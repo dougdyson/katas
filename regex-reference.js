8405370d118af18b7a40ebdaab858cef3e82877f
Unfamous Dyson <dougdyson@gmail.com>
2021-02-27 00:25:19 -0800

    convert first word to pig latin

--- regex-reference.js
+++ regex-reference.js
@@ -1,9 +1,9 @@
-let re1 = /ab+c/; //?
+const re1 = /ab+c/; //?
 
-let re2 = new RegExp('ab+c'); //?
+const re2 = new RegExp('ab+c'); //?
 
-let myRe = /d(b+)d/g;
-let myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz'); //?
+const myRe = /d(b+)d/g;
+const myArray = myRe.exec('bbbbbbbddddcdbbdbsbdz'); //?
 
 myArray[1]; //?
 
@@ -12,11 +12,11 @@ const phrase = / /g;
 const space = phrase.exec('Hello World!'); //?
 
 // find word
-const p = "Hello World!";
+const s = "Hello World!";
 const r = / /g;
-const i = r.exec(p).index; //?
-const w = p.slice(0, i); //?
-
-
+const i = r.exec(s).index; //?
 // respell word
+const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase() + 'ay'; //?
+
+// The above looks like a good candidate for reduce
 
