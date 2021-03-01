affd7881fd8cdb9ed836bde71b7621645590cdbe
Unfamous Dyson <dougdyson@gmail.com>
2021-03-01 01:15:23 -0800

    format code

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -1,5 +1,7 @@
 function pigIt(s) {
-  return s.split(' ').reduce((a, v) => a += v.slice(1) + v.slice(0,1) + 'ay ', '').trim();
+  return s.split(' ')
+          .reduce((a, v) => a += v.slice(1) + v.slice(0,1) + 'ay ', '')
+          .trim();
 }
 
 pigIt("hello world boom"); //?
