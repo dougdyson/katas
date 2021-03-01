fef94867fabab9090ec3c69205a3d20d2a40b3f6
Unfamous Dyson <dougdyson@gmail.com>
2021-03-01 00:54:03 -0800

    beware coding

--- regex-reference.js
+++ regex-reference.js
@@ -1,12 +1,17 @@
-const s = "Hello World, Hello!";
+const s = "hello world boom";
 // find space
-const r = /\s/g;
-// find word
-const i = r.exec(s).index; //?
-// respell word
-const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase().concat('ay'); //?
+// const r = /\s/g;
+// // find word
+// const i = r.exec(s).index; //?
+// // respell word
+// const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase().concat('ay'); //?
 
 // The above looks like a good candidate for reduce
 
-const pw = s.split(' ').reduce((acc,v,x,arr) =>  ) //?
+//s.split(' '); //?
+//s;
 
+const pw = s.split(' ')
+            .reduce((a, v) => a += v.slice(2) + v.slice(0,2) + 'ay ', s.slice(2) + s.slice(0,1) + 'ay '); //?
+
+pw;
