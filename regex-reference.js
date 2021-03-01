b97e8b5d41ab9d2187f559dff6ba328f53767c26
Unfamous Dyson <dougdyson@gmail.com>
2021-02-28 23:54:50 -0800

    code in progress - beware!

--- regex-reference.js
+++ regex-reference.js
@@ -1,10 +1,12 @@
-const s = "Hello World!";
+const s = "Hello World, Hello!";
 // find space
-const r = / /g;
+const r = /\s/g;
 // find word
 const i = r.exec(s).index; //?
 // respell word
-const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase() + 'ay'; //?
+const pigWord = s.slice(1, i) + s.slice(0, 1).toLowerCase().concat('ay'); //?
 
 // The above looks like a good candidate for reduce
+
+const pw = s.split(' ').reduce((acc,v,x,arr) =>  ) //?
 
