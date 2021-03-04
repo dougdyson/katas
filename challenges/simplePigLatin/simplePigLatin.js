4dd6225514b577df8b13fb768d6a1da43713cddd
Unfamous Dyson <dougdyson@gmail.com>
2021-03-04 05:45:34 -0800

    add refactor strategy comments

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -3,6 +3,12 @@ function pigIt(s) {
           .reduce((a, v) => /\w/.test(v) ? a += v.slice(1) + v.slice(0,1) + 'ay' : a += v, '');
 }
 
+// The above V1 is not too bad. However, after submitting this solution,
+// I reviewed other solutions which used regex much more effectively with
+// much less code.
+//
+// Will refactor with more effective regex/less code.
+
 pigIt("O tempora o mores !");
 
 module.exports = pigIt;
