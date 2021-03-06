7a17d209c9b6f51fcbd8eb29599fa1ecadd49fa5
Unfamous Dyson <dougdyson@gmail.com>
2021-03-05 23:11:01 -0800

    begin refactor

--- challenges/simplePigLatin/simplePigLatin.js
+++ challenges/simplePigLatin/simplePigLatin.js
@@ -1,4 +1,4 @@
-function pigIt(s) {
+function pigItV1(s) {
   return s.split(/\b/)
           .reduce((a, v) => /\w/.test(v) ? a += v.slice(1) + v.slice(0,1) + 'ay' : a += v, '');
 }
@@ -8,6 +8,12 @@ function pigIt(s) {
 // much less code.
 //
 // Will refactor with more effective regex/less code.
+
+
+function pigIt(s){
+  // s.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+  return s.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3") //?
+}
 
 pigIt("O tempora o mores !");
 
