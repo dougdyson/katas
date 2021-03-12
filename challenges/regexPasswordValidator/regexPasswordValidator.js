803ea23722c51ab5aaa3872fbce3decb174c6246
Unfamous Dyson <dougdyson@gmail.com>
2021-03-12 00:18:07 -0800

    add comment for potential approach

--- challenges/regexPasswordValidator/regexPasswordValidator.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.js
@@ -2,6 +2,8 @@ function validate(password) {
   return password.length <=6 
          ? /((put answer here))/.test(password)
          : false;
+
+// try incorporating the length validation as part of regex
 }
 
 module.exports = validate;
