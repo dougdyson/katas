abb903d5ab47f9f565e87e2d33c3d33da3cf221c
Unfamous Dyson <dougdyson@gmail.com>
2021-03-12 00:04:16 -0800

    add and pass test for non-alphanumeric characters

--- challenges/regexPasswordValidator/regexPasswordValidator.test.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.test.js
@@ -1,4 +1,4 @@
-/* https://www.codewars.com/kata/52e1476c8147a7547a000811/javascript
+/* https://www.codewars.com/kata/52e1476c8147a7547a000811/train/javascript
 
 - At least six characters long
 - contains a lowercase letter
@@ -13,3 +13,18 @@ const validate = require('./regexPasswordValidator');
 test('checks for empty string', () => {
   expect(validate('')).toBe(false);
 });
+
+test('checks for non-alphanumeric', () => {
+  expect(validate('!')).toBe(false);
+});
+
+/*
+Test.expect(validate('djI38D55'), 'djI38D55 - Expected true');
+Test.expect(!validate('a2.d412'), 'a2.d412 - Expected false');
+Test.expect(!validate('JHD5FJ53'), 'JHD5FJ53 - Expected false');
+Test.expect(!validate('!fdjn345'), '!fdjn345 - Expected false');
+Test.expect(!validate('jfkdfj3j'), 'jfkdfj3j - Expected false');
+Test.expect(!validate('123'), '123 - Expected false');
+Test.expect(!validate('abc'), 'abc - Expected false');
+Test.expect(validate('Password123'), 'Password123 - Expected true');
+*/
