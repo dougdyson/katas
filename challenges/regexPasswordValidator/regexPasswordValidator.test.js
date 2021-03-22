d596d64f4f688e2f4457032d1324bc53ed9c8b6a
Unfamous Dyson <dougdyson@gmail.com>
2021-03-22 00:04:11 -0700

    refactor in progress - caution

--- challenges/regexPasswordValidator/regexPasswordValidator.test.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.test.js
@@ -18,15 +18,17 @@ test('checks for non-alphanumeric', () => {
   expect(validate('!')).toBe(false);
   expect(validate('a2.d412')).toBe(false);
   expect(validate('!fdjn345')).toBe(false);
+  expect(validate('fdjn3!')).toBe(false);
 });
 
-test('checks valid 6 alphanumeric character password', () => {
-  expect(validate('djI38D55')).toBe(true);
-  expect(validate('Password123')).toBe(true);
+test('checks valid alphanumeric character password', () => {
+  expect(validate('djiI38')).toBe(true);
+  expect(validate('123')).toBe(true);
+  expect(validate('abc')).toBe(true);
 });
 
 test('checks invalid alphanumeric character password with too many characters', () => {
+  expect(validate('Password123')).toBe(false);
   expect(validate('JHD5FJ53')).toBe(false);
   expect(validate('jfkdfj3j')).toBe(false);
 });
-
