b39433ae559ac6a36903b73d478fb64f01befd6d
Unfamous Dyson <dougdyson@gmail.com>
2021-03-27 00:08:35 -0700

    standardize tests

--- challenges/regexPasswordValidator/regexPasswordValidator.test.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.test.js
@@ -16,10 +16,9 @@ test('checks for empty string', () => {
 
 test('checks for non-alphanumeric', () => {
   expect(validate('!')).toBe(false);
-  expect(validate('a2.d412')).toBe(false);
-  expect(validate('!fdjn345')).toBe(false);
-  expect(validate('!23abc')).toBe(false);
-  expect(validate('fdjn3!')).toBe(false);
+  expect(validate('a1.b2c')).toBe(false);
+  expect(validate('!abcD1')).toBe(false);
+  expect(validate('abC12!')).toBe(false);
 });
 
 test('checks invalid alphanumeric character password with missing case', () => {
@@ -35,8 +34,8 @@ test('checks valid alphanumeric character password', () => {
 
 test('checks invalid alphanumeric character password with too many characters', () => {
   expect(validate('Password123')).toBe(false);
-  expect(validate('JHD5FJ53')).toBe(false);
-  expect(validate('jfkdfj3j')).toBe(false);
-  expect(validate('!fkdfj3j')).toBe(false);
-  expect(validate('j!kdfj3j')).toBe(false);
+  expect(validate('abC1234')).toBe(false);
+  expect(validate('1234abC')).toBe(false);
+  expect(validate('abC123!')).toBe(false);
+  expect(validate('!234abC')).toBe(false);
 });
