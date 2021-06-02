e23a08314b5695f22a44513e6c1b39443a48f9db
Unfamous Dyson <dougdyson@gmail.com>
2021-06-01 23:52:40 -0700

    refactoring

--- challenges/regexPasswordValidator/regexPasswordValidator.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.js
@@ -1,53 +1,53 @@
-function validateV1(password) {
-  let regexp = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
-  return password.length > 6
-         ? regexp.test(password)
+function validate(password) {
+  let regexp = /(?=.{8,})/;
+  return regexp.test(password)
+         ? true
          : false
 }
 
-function validate(password) {
+// function validate(password) {
   
-  if (password.length < 6) return false;
+//   if (?=.{8,}./test(password)) return false;
 
-  if (function validate(password) {
+//   if (function validate(password) {
   
-  if (password.length < 6) return false;
+//   if (password.length < 6) return false;
 
-  if (!/^(?=.*[a-z])/.test(password)) {
-    return false;
-  }
+//   if (!/^(?=.*[a-z])/.test(password)) {
+//     return false;
+//   }
 
-  if (!/[A-Z]/.test(password)) {
-    return false;
-  }
+//   if (!/[A-Z]/.test(password)) {
+//     return false;
+//   }
 
-  if (!/[0-9]/.test(password)) {
-    return false;
-  }
+//   if (!/[0-9]/.test(password)) {
+//     return false;
+//   }
 
-  if (/[.!?\\-]/.test(password)) {
-    return false;
-  }
+//   if (/[.!?\\-]/.test(password)) {
+//     return false;
+//   }
 
-  return true;
-}/.test(password)) {
-    return false;
-  }
+//   // return true;
+//   // if (/.test(password)) {
+//   //   return false;
+//   // }
 
-  if (!/[A-Z]/.test(password)) {
-    return false;
-  }
+//   if (!/[A-Z]/.test(password)) {
+//     return false;
+//   }
 
-  if (!/[0-9]/.test(password)) {
-    return false;
-  }
+//   if (!/[0-9]/.test(password)) {
+//     return false;
+//   }
 
-  if (/[.!?\\-]/.test(password)) {
-    return false;
-  }
+//   if (/[.!?\\-]/.test(password)) {
+//     return false;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
 // alphanumeric only: \w{1,6}
 // one upper case: [A-Z]
@@ -58,19 +58,19 @@ function validate(password) {
 // How to concatenate/combine regex parameters?
 
 
-validateV1('a1.b2c') //?
-validateV1('!aB123') //?
-validateV1('ab123!') //?
-validateV1('!@#$%^') //?
+// validateV1('a1.b2c') //?
+// validateV1('!aB123') //?
+// validateV1('ab123!') //?
+// validateV1('!@#$%^') //?
 validate('abcdefg') //?
-validateV1('abcdefg1B') //?
+// validateV1('abcdefg1B') //?
 validate('123456') //?
-validateV1('abc123') //?
+// validateV1('abc123') //?
 validate('abcdef') //?
 validate('123') //?
 validate('abc') //?
 validate('AbC1') //?
-validateV1('abC123') //?
+// validateV1('abC123') //?
 validate('123aBC') //?
 
 
