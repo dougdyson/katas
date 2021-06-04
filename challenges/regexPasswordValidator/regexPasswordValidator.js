2ce3d90cc11d725d47eb35af625393c5034d03c0
Unfamous Dyson <dougdyson@gmail.com>
2021-06-03 23:35:37 -0700

    beware of code

--- challenges/regexPasswordValidator/regexPasswordValidator.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.js
@@ -5,72 +5,19 @@ function validate(password) {
          : false
 }
 
-// function validate(password) {
-  
-//   if (?=.{8,}./test(password)) return false;
-
-//   if (function validate(password) {
-  
-//   if (password.length < 6) return false;
-
-//   if (!/^(?=.*[a-z])/.test(password)) {
-//     return false;
-//   }
-
-//   if (!/[A-Z]/.test(password)) {
-//     return false;
-//   }
-
-//   if (!/[0-9]/.test(password)) {
-//     return false;
-//   }
-
-//   if (/[.!?\\-]/.test(password)) {
-//     return false;
-//   }
-
-//   // return true;
-//   // if (/.test(password)) {
-//   //   return false;
-//   // }
-
-//   if (!/[A-Z]/.test(password)) {
-//     return false;
-//   }
-
-//   if (!/[0-9]/.test(password)) {
-//     return false;
-//   }
-
-//   if (/[.!?\\-]/.test(password)) {
-//     return false;
-//   }
-
-//   return true;
-// }
-
-// alphanumeric only: \w{1,6}
-// one upper case: [A-Z]
-// one lower case: [a-z]
-// one number: [0-9]
-// no special characters: 
-// <=6 length: {1.6}
-// How to concatenate/combine regex parameters?
-
-
-// validateV1('a1.b2c') //?
-// validateV1('!aB123') //?
-// validateV1('ab123!') //?
-// validateV1('!@#$%^') //?
+validate('a1.b2c') //?
+validate('!aB123') //?
+validate('ab123!') //?
+validate('!@#$%^') //?
 validate('abcdefg') //?
-// validateV1('abcdefg1B') //?
+validate('abcdefg1B') //?
 validate('123456') //?
-// validateV1('abc123') //?
+validate('abc123') //?
 validate('abcdef') //?
 validate('123') //?
 validate('abc') //?
 validate('AbC1') //?
-// validateV1('abC123') //?
+validate('abC123') //?
 validate('123aBC') //?
 validate('ABCDEF') //?
 validate('abcDEF') //?
