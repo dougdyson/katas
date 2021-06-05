214719e4a6c5139f0735d56ac9dfbfeab80b1916
Unfamous Dyson <dougdyson@gmail.com>
2021-06-04 18:24:39 -0700

    clean up code and remove temp tests

--- challenges/regexPasswordValidator/regexPasswordValidator.js
+++ challenges/regexPasswordValidator/regexPasswordValidator.js
@@ -1,25 +1,3 @@
-function validate(password) {
-  
-  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\w{6,}$/.test(password)
-
-}
-
-validate('a1.b2c') //?
-validate('!aB123') //?
-validate('ab123!') //?
-validate('!@#$%^aA') //?
-validate('abcdefg') //?
-validate('abcdefg1B') //?
-validate('123456') //?
-validate('abc123') //?
-validate('abcdef') //?
-validate('123') //?
-validate('abc') //?
-validate('AbC1') //?
-validate('abC123') //?
-validate('123aBC') //?
-validate('ABCDEF') //?
-validate('abcDEF') //?
-
+const validate = password => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\w{6,}$/.test(password);
 
 module.exports = validate;
