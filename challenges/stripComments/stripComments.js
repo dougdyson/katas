6a21115509f03bcf1a47005a6f53f76a47835930
Unfamous Dyson <dougdyson@gmail.com>
2021-06-06 23:34:01 -0700

    update reference

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -2,9 +2,9 @@ const strippedComments = (input, markers) => {
 
   // for each marker, slice characters following marker until new line
   // i.e. strippedComments("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]) = "apples, plums\npears\noranges"
-
   
-  return input;
+  // good candidate for replace
+
 }
 
 module.exports = strippedComments;
