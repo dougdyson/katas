aa44b953b295176334d6a25178b7a613480844ae
Unfamous Dyson <dougdyson@gmail.com>
2021-06-14 23:53:55 -0700

    getting warmer

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,24 +1,10 @@
 const strippedComments = (input, markers) => {
 
-  // for each marker, slice characters following marker until new line
-  // i.e. strippedComments("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]) = "apples, plums\npears\noranges"
-  // good candidate for replace
-
-  // replace(look for, replace with)
-  // look for = marker
-  // replace with = ''
-
-  // do without regex first, then apply regex
-  // use string or convert to array?
-  // string
-  let marker = markers[0];
-
-  input = input.replace(marker, ''); //?
-
-  return [...input].join(('')) //?
+  let marker = markers[0]; //?
+  return [...input].reduce((a, v, i, arr) => arr[i] === marker ? a = a : a = a + arr[i]  ) //?
 
 }
 
-strippedComments("apples, plums | and bananas", '|'); //? 
+strippedComments("apples, plums and | bananas", '|'); //? 
 
 module.exports = strippedComments;
