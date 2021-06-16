f763a0968d3d9e916b80ff558ee34baad687491d
Unfamous Dyson <dougdyson@gmail.com>
2021-06-15 23:48:30 -0700

    closer!

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,10 +1,13 @@
 const strippedComments = (input, markers) => {
 
-  let marker = markers[0]; //?
-  return [...input].reduce((a, v, i, arr) => arr[i] === marker ? a = a : a = a + arr[i]  ) //?
+  // do for one marker first, then do for array of markers
 
+  const marker = markers[0]; //?
+  const comments = [...input].reduce((a, v, i, arr) => v === marker ? a : a + v ) //?
+
+  return comments //?
 }
 
-strippedComments("apples, plums and | bananas", '|'); //? 
+strippedComments("apples, plums | and bananas", '|'); //? 
 
 module.exports = strippedComments;
