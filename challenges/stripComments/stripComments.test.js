30791ba483265d300892a70ed98727b6b3de10a3
Unfamous Dyson <dougdyson@gmail.com>
2021-06-15 23:49:36 -0700

    update tests

--- challenges/stripComments/stripComments.test.js
+++ challenges/stripComments/stripComments.test.js
@@ -3,10 +3,6 @@ const strippedComments = require('./stripComments.js');
 // checkComments("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"], "apples, plums\npears\noranges")
 // checkComments("Q @b\nu\ne -e f g", ["@", "-"], "Q\nu\ne")
 
-test('invalid empty string', () => {
-  expect(strippedComments('','')).toBe('');
-});
-
 test('valid string with marker', () => {
   expect(strippedComments('apples, plums # and bananas\npears\noranges #applesauce',['#']))
     .toBe('apples, plums\npears\noranges');
