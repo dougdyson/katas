6dee891582cb066184bb0cdef1c94d51adc8e423
Unfamous Dyson <dougdyson@gmail.com>
2021-06-16 23:28:09 -0700

    refactor simple solution

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,13 +1,7 @@
 const strippedComments = (input, markers) => {
 
-  // do for one marker first, then do for array of markers
-
-  const marker = markers[0]; //?
-  //let comments = [...input].reduce((a, v, i, arr) => v === marker ? a : a + v ) //?
-
-  const commentStart = [...input].findIndex(char => char === marker);
+  return input.slice(markers[0], [...input].findIndex(char => char === markers[0]))
   
-  return input.slice(0, commentStart) //?
 }
 
 strippedComments("apples, plums | and bananas", '|'); //? 
