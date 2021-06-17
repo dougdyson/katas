66da4973a7cdc45ad25f433ff7b60268894480a8
Unfamous Dyson <dougdyson@gmail.com>
2021-06-16 23:48:49 -0700

    refactor simple solution and clean code

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,15 +1,11 @@
-const strippedComments = (input, markers) => {
-
-  // add multi-line
-  // add array of markers
-
-  // array method, leaving for temp ref
-  // return input.slice(markers[0], [...input].findIndex(char => char === markers[0]))
-  
-  return input.slice(0, input.indexOf(markers[0])) //?
-
-}
-
-strippedComments("apples, plums | and bananas", '|'); //? 
+const strippedComments = (input, markers) => input.slice(0, input.indexOf(markers[0])) //?
 
 module.exports = strippedComments;
+
+strippedComments('apples, plums | and bananas', '|'); //? 
+
+// [] multi-line
+// [] array of markers
+
+// array method, leaving for temp ref:
+// return input.slice(markers[0], [...input].findIndex(char => char === markers[0]))
