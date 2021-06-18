8965c28aeca10dad0b5abcca38267834ea251fc3
Unfamous Dyson <dougdyson@gmail.com>
2021-06-17 19:00:12 -0700

    clean up code

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -7,12 +7,6 @@ const strippedComments = (input, markers) => {
 }
 module.exports = strippedComments;
 
-strippedComments('apples| and bananas', ['|']); //?
-strippedComments('apples| and bananas\n2nd line\n3rd line!', ['|']); //?
-
 // [X] simple case: single line & one marker
 // [X] multi-line
 // [] array of markers
-
-// array method, leaving for temp ref:
-// return input.slice(markers[0], [...input].findIndex(char => char === markers[0]))
