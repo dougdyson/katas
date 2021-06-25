c3b1270ccfdf697f699bc9da43fe15d398305328
Unfamous Dyson <dougdyson@gmail.com>
2021-06-25 16:08:54 -0700

    clean code

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,6 +1,7 @@
 const strippedComments = (input, markers) => {
 
   const slice = (a, m) => a.split(/\n/).reduce((a,v) => v.includes(m) ? a += '\n' + v.slice(0, v.indexOf(m)).trim() : a += '\n' + v, '')
+  
   return markers.reduce((a, m) => a = slice(a, m), input).trim();
 
 }
