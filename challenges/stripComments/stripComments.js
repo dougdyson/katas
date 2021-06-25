370607c87e42756909a4adc5bf11816be610caf0
Unfamous Dyson <dougdyson@gmail.com>
2021-06-24 23:42:44 -0700

    refactor in progress

--- challenges/stripComments/stripComments.js
+++ challenges/stripComments/stripComments.js
@@ -1,16 +1,15 @@
-const strippedComments = (input, markers) => {
+const strippedCommentsV1 = (input, markers) => {
 
   const slice = (a, m) => a.split(/\n/).reduce((a,v) => v.includes(m) ? a += '\n' + v.slice(0, v.indexOf(m)).trim() : a += '\n' + v, '')
   return markers.reduce((a, m) => a = slice(a, m), input).trim();
 
 }
 
-const strippedCommentsV2 = (input, markers) => input.replace( new RegExp(" [" + markers.join(',') + "].+", "gi"), '' );
-
 module.exports = strippedComments;
 
 // refactor examples:
 // const solution = (input, markers) => input.replace( new RegExp(" [" + markers.join(',') + "].+", "gi"), '' );
+// ^ doesn't pass tests
 
 // function solution(input, markers) {
 //   return input.replace(/(\s\W\w+).*|(\s\W\s?\w+\s?\w+)/g, "");
