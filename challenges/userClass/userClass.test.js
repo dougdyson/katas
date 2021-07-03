5d075e5c29aa796bb1644877884653811fdf0609
Unfamous Dyson <dougdyson@gmail.com>
2021-07-02 18:22:57 -0700

    add test rank completes same ranked activity

--- challenges/userClass/userClass.test.js
+++ challenges/userClass/userClass.test.js
@@ -9,3 +9,8 @@ test('Class successfully created with default progress of 0', () => {
   const NewUser = new User(); //?
   expect(NewUser.progress).toBe(0)
 });
+
+test('Rank 8 completes Rank 8 activity for a progress of 10', () => {
+  const NewUser = new User().incProgress(8); //?
+  expect(NewUser.progress).toBe(10)
+});
