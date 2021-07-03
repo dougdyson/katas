59aa8eb10879fd56caa05d18c2b32125e089cb71
Unfamous Dyson <dougdyson@gmail.com>
2021-07-03 00:50:05 -0700

    increment rank if rank = 0

--- challenges/userClass/userClass.js
+++ challenges/userClass/userClass.js
@@ -9,7 +9,7 @@ class User {
     else if (this.rank - a < 0) this.progress += ((this.rank - a) * -10);
 
     if (this.progress > 100) {
-      this.rank += 1;
+      this.rank + 1 != 0 ? this.rank += 1 : this.rank = 1;
       this.progress = this.progress - 100;
     }
   }
