12774040211108050201437538ac9389fa4ffa61
Unfamous Dyson <dougdyson@gmail.com>
2021-11-11 00:44:46 -0800

    add sequential test

--- challenges/rangeExtraction/rangeExtraction.test.js
+++ challenges/rangeExtraction/rangeExtraction.test.js
@@ -3,3 +3,7 @@ const rangeExtraction = require('./rangeExtraction');
 test('converts a non-sequential array to a string with each array element comma seperated', () => {
   expect(rangeExtraction([-1,1,3])).toBe("-1,1,3")
 });
+
+test('converts a sequential array to a string indicating a range', () => {
+  expect(rangeExtraction([-1,0,-1])).toBe('-1-1')
+});
