d0e7cd8c016a055457d8e965888f8b6036d471ef
Unfamous Dyson <dougdyson@gmail.com>
2021-11-10 23:32:47 -0800

    add creating range test

--- challenges/rangeExtraction/rangeExtraction.test.js
+++ challenges/rangeExtraction/rangeExtraction.test.js
@@ -1,5 +1,9 @@
 const rangeExtraction = require('./rangeExtraction');
 
 test('returns a string', () => {
-  expect.stringContaining(rangeExtraction([1]))
+  expect.stringContaining(rangeExtraction([1]));
+});
+
+test('creates a range from consecutive numbers', () => {
+  expect(rangeExtraction([-1,0,1])).toBe("-1-1")
 });
