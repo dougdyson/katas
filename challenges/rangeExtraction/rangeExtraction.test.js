29ccfdaa2782dda7749a59462078c4616e099cbc
Unfamous Dyson <dougdyson@gmail.com>
2021-11-11 01:09:41 -0800

    expand tests

--- challenges/rangeExtraction/rangeExtraction.test.js
+++ challenges/rangeExtraction/rangeExtraction.test.js
@@ -1,9 +1,9 @@
 const rangeExtraction = require('./rangeExtraction');
 
 test('converts a non-sequential array to a string with each array element comma seperated', () => {
-  expect(rangeExtraction([-1,1,3])).toBe("-1,1,3")
+  expect(rangeExtraction([-3,-1,1,3])).toBe("-3,-1,1,3")
 });
 
 test('converts a sequential array to a string indicating a range', () => {
-  expect(rangeExtraction([-1,0,-1])).toBe('-1-1')
+  expect(rangeExtraction([-2,-1])).toBe('-2-1')
 });
