d9fdead26ab3ae3110796dfb442dac96ea0d106f
Unfamous Dyson <dougdyson@gmail.com>
2021-11-11 21:57:40 -0800

    clean code

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -1,8 +1,5 @@
 const rangeExtraction =  (r) => {
   return r.reduce((a,v,i,arr) => arr[i+1] - v != 1 ? a += `,${String(v)}` : a += `-${String(v)}`)
-
-  // return r.reduce((a,v,i,arr) => { 
-  //   return arr[i+1]-v === 1 ? a += `-${String(v)}` : a += `,${String(v)}`})
 }
 
 module.exports = rangeExtraction;
