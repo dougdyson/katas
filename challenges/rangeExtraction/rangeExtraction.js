d24d0578fc8ee85662ffe7228fc9d77bd2fba05a
Unfamous Dyson <dougdyson@gmail.com>
2021-11-13 03:40:36 -0800

    bang head

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -1,11 +1,9 @@
 const rangeExtraction =  (r) => {
   // return r.reduce((a,v,i,arr) => arr[i+1] - v != 1 ? a += `,${String(v)}` : a += `-${String(v)}`);
 
-  // [-2,-1,0,1,2]
-
   const range = r.reduce((a,v,i,arr) => {
-    if (i === 0) a = `${v}-`
-    else if (arr[i] - arr[i+1] != -1 ) a += `-${v}`
+    if (i === 0) a += `${v}-`
+    else if (arr[i+1] - v != 1 ) a += `-${v}`
     return a
   })
 
@@ -14,5 +12,6 @@ const rangeExtraction =  (r) => {
 }
 
 const test = rangeExtraction([-2,-1,0,1,2]);
+test
 
 module.exports = rangeExtraction
