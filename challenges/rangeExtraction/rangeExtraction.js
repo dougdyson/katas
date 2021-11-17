5e4abf46bfd8336851db7ee7e019c8b51b495cac
Unfamous Dyson <dougdyson@gmail.com>
2021-11-16 17:28:17 -0800

    review additional tests

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -3,22 +3,20 @@ const rangeExtraction =  (r) => {
   let range = [r[0]];
   
   for (let i = 1; i < r.length; i++) {
-    const current = r[i];
-    const previous = r[i-1];
-    const lastRangeIndex = range.length-1;
-    console.log(current - previous);
-    (current - previous != 1) ? range.push(current) : range[lastRangeIndex] = range[lastRangeIndex].toString().slice(0,2) + `-${current}`
-    
+    const c = r[i];
+    const p = r[i-1];
+    const li = range.length-1;
+    (c - p != 1) ? range.push(c) : range[li] = range[li].toString().slice(0,2) + `-${c}`
   }
 
   return range.toString();
 }
 
-let test = rangeExtraction([-2,-1,0,1,2]);
+let test = rangeExtraction([-2,-1,0,1,2,4]);
 test;
-// test = rangeExtraction([-2,-1,0,2,4,5]);
-// test;
-// test = rangeExtraction([-2,-1,0,1,2,4,5,6]);
-// test
+test = rangeExtraction([-2,-1,0,2,4,5]);
+test;
+test = rangeExtraction([-2,-1,0,1,2,4,5,6]);
+test
 
 module.exports = rangeExtraction
