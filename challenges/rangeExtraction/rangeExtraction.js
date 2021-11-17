a41bb17e0c65bc356784c879248d76515c80182a
Unfamous Dyson <dougdyson@gmail.com>
2021-11-17 04:03:15 -0800

    bang head

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -4,13 +4,22 @@ const rangeExtraction =  (r) => {
 
   for (let i = 1; i < r.length; i++) {
     const li = range.length-1; // last index of range array
-    (r[i] - r[i-1] != 1) ? range.push(r[i]) 
-                         : range[li] = range[li].toString().slice(0,2) + `-${r[i]}`
+    if (r[i] - r[i-1] != 1) {
+      range.push(r[i])
+    }
+    else {
+      const lookingFor = r[i-1].toString()
+      lookingFor
+      const cut = range[li].toString().indexOf(lookingFor)
+      cut
+      range[li] = range[li].toString().slice(cut) + `-${r[i]}`;
+    }
   }
+  range
   return range.toString();
 }
 
-let test = rangeExtraction([-2,-1,0,1,2,4,5,]);
-test;
+rangeExtraction([-20,-19,-18,-4,-3,-2,-1,0,1,2,3,7,8,9,11,12,13,20,21,22,23]);
+
 
 module.exports = rangeExtraction
