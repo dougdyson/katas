38bc8683a10e42c78d35248b135d843a4b477cdf
Unfamous Dyson <dougdyson@gmail.com>
2021-11-17 05:48:47 -0800

    solve challenge

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -1,21 +1,24 @@
 const rangeExtraction =  (r) => {
-  // initialize output variable
+  // initialize closure output variable
   let range = [r[0]];
 
   for (let i = 1; i < r.length; i++) {
+    // check if current and previous array element are sequential
     if (r[i] - r[i-1] != 1) {
       range.push(r[i])
     } else {
       const li = range.length-1; // last index of range array
-      // find the last '-'
-      const sliceFrom = range[li].toString().lastIndexOf('-')
-      sliceFrom
-      // don't slice < 0
-      if (sliceFrom <= 0){
-        sliceFrom
-        range[li] = range[li].toString() + `-${r[i]}`;
+      const sliceTo = range[li].toString().lastIndexOf('-') // find the last '-'
+      if (sliceTo <= 0){ // first array element is '-' char or '-' is not found
+        range[li] += `-${r[i]}`
       } else {
-        range[li] = range[li].toString().slice(sliceFrom) + `-${r[i]}`
+        if (r[i] < 1){
+          // account spacing for negative numbers
+          range[li] = range[li].toString().slice(0,sliceTo-1) + `-${r[i]}` 
+        } else {
+          // account spacing for positive numbers
+          range[li] = range[li].toString().slice(0,sliceTo+1) + `${r[i]}`
+        }
       }
     }
   }
@@ -23,7 +26,7 @@ const rangeExtraction =  (r) => {
   return range.toString();
 }
 
-rangeExtraction([-20,-19,-18,-3,-2,-1,0,2,3,4,10,11,12]);
+rangeExtraction([2,4,5,6,10,11,12,13]);
 
 
 module.exports = rangeExtraction
