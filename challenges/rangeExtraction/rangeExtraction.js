4cf8715838df5775775a302487dbd4cec32af084
Unfamous Dyson <dougdyson@gmail.com>
2021-11-16 21:39:18 -0800

    concisify!

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -1,15 +1,11 @@
 const rangeExtraction =  (r) => {
-  // return r.reduce((a,v,i,arr) => arr[i+1] - v != 1 ? a += `,${String(v)}` : a += `-${String(v)}`);
-  
   // initialize output variable
   let range = [r[0]];
 
   for (let i = 1; i < r.length; i++) {
-    const cv = r[i];   // current value
-    const pv = r[i-1]; // previous value
     const li = range.length-1; // last index of range
-    (cv - pv != 1) ? range.push(cv) 
-                   : range[li] = range[li].toString().slice(0,2) + `-${cv}`
+    (r[i] - r[i-1] != 1) ? range.push(r[i]) 
+                         : range[li] = range[li].toString().slice(0,2) + `-${r[i]}`
   }
   return range.toString();
 }
