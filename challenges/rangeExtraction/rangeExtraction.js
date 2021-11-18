4c7ddaccec486f67f74e8ed23989b1c28af51cbc
Unfamous Dyson <dougdyson@gmail.com>
2021-11-17 21:34:07 -0800

    Bang head

--- challenges/rangeExtraction/rangeExtraction.js
+++ challenges/rangeExtraction/rangeExtraction.js
@@ -4,28 +4,29 @@ const rangeExtraction =  (r) => {
 
   for (let i = 1; i < r.length; i++) {
     // check if current and previous array element are sequential
-    if (r[i] - r[i-1] != 1) {
+    if (Math.abs(r[i] - r[i-1]) != 1) {
       range.push(r[i]);
     } else {
-      const li = range.length-1; // last index of range array
-      const sliceTo = range[li].toString().lastIndexOf('-') // find the last '-'
+      const lastIndex = range.length-1;
+      const sliceTo = range[lastIndex].toString().lastIndexOf('-') // find the last '-'
       if (sliceTo <= 0){ // first array element is '-' char or '-' is not found
-        range[li] += `-${r[i]}`
+        range[lastIndex] += `-${r[i]}`
       } else {
         if (r[i] < 1){
           // account spacing for negative numbers
-          range[li] = range[li].toString().slice(0,sliceTo-1) + `-${r[i]}` 
+          range[lastIndex] = range[lastIndex].toString().slice(0,sliceTo-1) + `-${r[i]}` 
         } else {
           // account spacing for positive numbers
-          range[li] = range[li].toString().slice(0,sliceTo+1) + `${r[i]}`
+          range[lastIndex] = range[lastIndex].toString().slice(0,sliceTo+1) + `${r[i]}`
         }
       }
     }
   }
+  range
   return range.toString();
 }
 
-rangeExtraction([2,4,5,6,10,11,12,13]);
-
+re = rangeExtraction([-12,-6,-5,-4,-1,0,2,3,10,11,12,13])
+re
 
 module.exports = rangeExtraction
