e7c595a007f06a8eba40f4a0dc305008543c581b
Unfamous Dyson <dougdyson@gmail.com>
2022-03-30 01:02:02 -0700

    pass first test

--- challenges/firstNonRepeatingCharacter/firstNonRepeatingCharacter.js
+++ challenges/firstNonRepeatingCharacter/firstNonRepeatingCharacter.js
@@ -1,4 +1,15 @@
 const firstNonRepeatingCharacter = (s) => {
+  let match = ''
+
+  s.split('').reduce((acc,cv,idx) => {
+    (cv.includes(cv, idx + 1)) ? match = '' : match = cv
+  }, '')
+  
+  return match;
+}
+
+const firstNonRepeatingCharacterV1 = (s) => {
+  // copilot solution
   const charMap = {};
   for (let i = 0; i < s.length; i++) {
     if (charMap[s[i]]) {
