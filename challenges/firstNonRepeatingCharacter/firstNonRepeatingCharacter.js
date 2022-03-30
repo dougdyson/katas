98e9ca1170ef5cd59cc9ac8aa943225fbbd8b964
Unfamous Dyson <dougdyson@gmail.com>
2022-03-30 00:28:31 -0700

    copilot solution

--- challenges/firstNonRepeatingCharacter/firstNonRepeatingCharacter.js
+++ challenges/firstNonRepeatingCharacter/firstNonRepeatingCharacter.js
@@ -1,3 +1,18 @@
-const firstNonRepeatingCharacter = (s) => s;
+const firstNonRepeatingCharacter = (s) => {
+  const charMap = {};
+  for (let i = 0; i < s.length; i++) {
+    if (charMap[s[i]]) {
+      charMap[s[i]]++;
+    } else {
+      charMap[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (charMap[s[i]] === 1) {
+      return s[i];
+    }
+  }
+  return '';
+}
 
 module.exports = firstNonRepeatingCharacter;
