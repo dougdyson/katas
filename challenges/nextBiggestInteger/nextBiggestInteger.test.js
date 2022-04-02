5731efc8373decbdbd5e9a948565a1615673a6de
Unfamous Dyson <dougdyson@gmail.com>
2022-04-01 23:35:50 -0700

    update test description

--- challenges/nextBiggestInteger/nextBiggestInteger.test.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.test.js
@@ -1,11 +1,12 @@
 const nextBiggestInteger = require('./nextBiggestInteger');
 
-describe("Tests", () => {
-  it("test", () => {
+describe("Simple tests", () => {
+  it("Number shouldbe next highest value compromised from available integers", () => {
     expect(nextBiggestInteger(12)).toBe(21)
     expect(nextBiggestInteger(513)).toBe(531)
     expect(nextBiggestInteger(2017)).toBe(2071)
     expect(nextBiggestInteger(414)).toBe(441)
     expect(nextBiggestInteger(144)).toBe(414)
   });
+  
 });
