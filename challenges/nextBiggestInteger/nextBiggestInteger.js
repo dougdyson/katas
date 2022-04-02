c0db7c20285205b14be64105c5d33500fa07ec4e
Unfamous Dyson <dougdyson@gmail.com>
2022-04-02 00:41:52 -0700

    in progress

--- challenges/nextBiggestInteger/nextBiggestInteger.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.js
@@ -1,24 +1,25 @@
 const nextBiggestInteger = (int) => {
   
   // integer map for later iteration
-  const numMap = int.toString().split('').sort()
+  const numMap = int.toString().split('').sort() //?
 
   // maximum number possible
   const maxNum = numMap.reverse().join('') //?
 
   // get next index from numMap
-  const nextIndex = (i) => {
-    const currentIndex = numMap.indexOf(i);
-    
-    if (currentIndex === numMap.length - 1) return numMap[0]
-    
-    return nextIndex = currentIndex + 1;
-  }
+  const nextIndex = (i) => (numMap.indexOf(i) < numMap.length - 1) ? i++ : 0;
 
-  for (let index = int; index < maxNum; index++) {
-    const element = array[index];
-    
+  // reduce int to next highest number from numMap
+  const nextHighest = (int) => {  //?
+    let i = 0;
+    while (int > maxNum) {
+      // get next value from numMap
+      const nextNum = nextIndex(i);
+      // add next value to int
+      int += nextNum;
+    }
   }
+  return int
 }
 
 const test = nextBiggestInteger(2017) //?
