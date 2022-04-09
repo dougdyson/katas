4c95d504cff435d31b49e1495edf1edac20d3553
Unfamous Dyson <dougdyson@gmail.com>
2022-04-08 23:45:15 -0700

    add test

--- challenges/nextBiggestInteger/nextBiggestInteger.test.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.test.js
@@ -5,7 +5,7 @@ describe("Simple tests", () => {
     expect(nextBiggestInteger(12)).toBe(21)
     expect(nextBiggestInteger(513)).toBe(531)
     expect(nextBiggestInteger(2017)).toBe(2071)
-    // expect(nextBiggestInteger(414)).toBe(441)
+    expect(nextBiggestInteger(414)).toBe(441)
     // expect(nextBiggestInteger(144)).toBe(414)
   });
   
