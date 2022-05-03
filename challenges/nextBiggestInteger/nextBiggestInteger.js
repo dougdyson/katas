6f11e4df2cebdbe6ae242b07a090e6bd7cc5e77c
Unfamous Dyson <dougdyson@gmail.com>
2022-05-03 14:58:46 -0700

    passes first 3 Simple Tests

--- challenges/nextBiggestInteger/nextBiggestInteger.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.js
@@ -4,13 +4,31 @@ const nextBiggestInteger = int => {
   const digits = int.toString().split(''); //?
 
   // maximum number possible
-  const maxNum = parseInt(digits.sort().reverse().join('')); //?
-  int
+  const maxInt = parseInt(digits.sort().reverse().join('')); //?
 
   // if int is already the maximum possible number, return -1
-  if (int === maxNum) return -1; //?
+  if (int === maxInt) return -1; //?
+
+  // int + 1
+  let nextInt = int + 1; //?
+
+  // add +1 to int and check to see if all digits are in int in any order
+  while (nextInt <= maxInt) {
+    // create array of digits from nextInt
+    const nextIntDigits = nextInt.toString().split(''); //?
+    
+    // check if all digits are in int in any order
+    if (digits.every(digit => nextIntDigits.includes(digit))) {
+      return nextInt;
+    }
+
+    // increment nextInt
+    nextInt = nextInt + 1; //?
+  }
+
+
 }
 
-nextBiggestInteger(333); //?
+nextBiggestInteger(12); //?
 
 module.exports = nextBiggestInteger;
--- challenges/nextBiggestInteger/nextBiggestInteger.test.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.test.js
@@ -6,7 +6,7 @@ describe("Simple tests", () => {
     expect(nextBiggestInteger(513)).toBe(531)
     expect(nextBiggestInteger(2017)).toBe(2071)
     expect(nextBiggestInteger(414)).toBe(441)
-    expect(nextBiggestInteger(144)).toBe(414)
+    expect(nextBiggestInteger(144)).toBe(414) 
     expect(nextBiggestInteger(1234321)).toBe(1241233);
   });
 });
