5ef6766d376d610440afd4ea95a2d745a19dc03e
Unfamous Dyson <dougdyson@gmail.com>
2022-05-08 00:07:50 -0700

    add negative test

--- challenges/nextBiggestInteger/nextBiggestInteger.test.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.test.js
@@ -16,6 +16,7 @@ describe('Negative tests', () => {
     expect(nextBiggestInteger(333)).toBe(-1);
     expect(nextBiggestInteger(0)).toBe(-1);
     expect(nextBiggestInteger(1)).toBe(-1);
+    expect(nextBiggestInteger(2)).toBe(-1);
   });
 });
 
