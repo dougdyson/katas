4bb55710abc9417fc04a1198529f63d4b4d45fb5
Unfamous Dyson <dougdyson@gmail.com>
2022-08-27 09:56:30 -0700

    frankenstein's monster

--- challenges/nextBiggestInteger/nextBiggestInteger.js
+++ challenges/nextBiggestInteger/nextBiggestInteger.js
@@ -1,9 +1,9 @@
 const nextBiggestInteger = int => {
-
+  
   // VALIDATE INPUT
   // return -1 for empty string, null, undefined, NaN, or negative numbers
   if (int === '' || int === null || int === undefined || isNaN(int) || int < 0) return -1;
-
+  
   // convert int to array of digits
   const digits = int.toString().split(''); //?
 
@@ -16,15 +16,36 @@ const nextBiggestInteger = int => {
   // next integer from int
   let nextInt = int + 1; //?
 
+  
   // add +1 to int and check to see if all digits are in the array in any order
   while (nextInt < maxInt) {
     
+    // create temp array of digits
+    const tempDigits = digits //?
+
     // create array of digits from nextInt
     const nextIntDigits = nextInt.toString().split(''); //?
 
-    // check each digit in nextIntDigits to if it is in maxInt
-    nextIntDigits.findIndex((digit, idx) => maxInt);//?
-    
+    // check to see if all digits from nextIntDigits are in tempDigits
+    const allDigits = nextIntDigits.every(digit => tempDigits.includes(digit));
+    if (allDigits) {
+      
+      console.log(`all digits found for ${nextInt} in ${digits}`);
+      
+      digits.reduce((acc, digit) => {
+        console.log(digit)
+        // find first instance of digit in tempDigits
+        const index = tempDigits.indexOf(digit); //?
+        // remove digit from tempDigits
+        tempDigits.splice(index, 1); //?
+        console.log(`${tempDigits} and tempDigits length is ${tempDigits.length}`);
+      }, 0); //?
+
+      // if tempDigits is empty, all digits were found
+      if (tempDigits.length === 0) return nextInt; //?
+
+    }
+
     nextInt = nextInt + 1; //?
     
   };
@@ -33,6 +54,6 @@ const nextBiggestInteger = int => {
 }
 
 
-nextBiggestInteger(414); //?
+nextBiggestInteger(114); //?
 
 module.exports = nextBiggestInteger;
