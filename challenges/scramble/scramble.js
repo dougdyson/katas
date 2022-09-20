cc9473e18573fb2b0272952dda90a5cf58a291b2
Unfamous Dyson <dougdyson@gmail.com>
2022-09-20 11:40:15 -0700

    pass performance test

--- challenges/scramble/scramble.js
+++ challenges/scramble/scramble.js
@@ -1,4 +1,4 @@
-const scramble = (str1, str2) => {
+const scrambleCopilot = (str1, str2) => {
   let str1Arr = str1.split('');
   let str2Arr = str2.split('');
   let result = true;
@@ -11,5 +11,31 @@ const scramble = (str1, str2) => {
   });
   return result;
 }
+
+const scramble = (str1, str2) => {
+
+  // create map of str1
+  const str1Map = str1.split('').reduce((acc, letter) => {
+    acc[letter] = acc[letter] + 1 || 1;
+    return acc;
+  }
+  , {});
+
+  // create map of str2
+  const str2Map = str2.split('').reduce((acc, letter) => {
+    acc[letter] = acc[letter] + 1 || 1;
+    return acc;
+  }
+  , {});
+
+  // check if str2Map is a subset of str1Map
+  return Object.keys(str2Map).every((letter) => {
+    return str1Map[letter] >= str2Map[letter];
+  }
+  );
+
+}
+
+scramble('scriptjavx', 'javascript'); //?
 
 module.exports = scramble;
