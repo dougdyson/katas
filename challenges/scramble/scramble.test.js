c9dca6150b0adb3b77b9e2a471bea7e9c02958d6
Unfamous Dyson <dougdyson@gmail.com>
2022-09-20 10:26:24 -0700

    add remaining simple tests from spec

--- challenges/scramble/scramble.test.js
+++ challenges/scramble/scramble.test.js
@@ -2,6 +2,8 @@ const scramble = require('./scramble');
 
 describe("Simple tests", () => {
   it("returns true if a portion of str1 characters can be rearranged to match str2", () => {
-    expect(scramble('rkqodlw', 'world')).toBe(true);
+    expect(scramble('rkqodlw', 'world')).toBe(true)
+    expect(scramble('cedewaraaossoqqyt', 'codewars')).toBe(true)
+    expect(scramble('katas', 'steak')).toBe(false)
   })
 });
