2f6e65ab97e5289090c81ea1202ad21bc8be1cb2
Unfamous Dyson <dougdyson@gmail.com>
2022-10-11 15:34:06 -0700

    fiddling

--- challenges/pyramidSlideDown/highestValuePath.js
+++ challenges/pyramidSlideDown/highestValuePath.js
@@ -12,23 +12,24 @@ const highestValuePath = (pyramid) => {
 
   associativePyramid
 
-  // compare the column values of each row to determine the highest value path
+  // initialize loop variables
   let sum = associativePyramid[0][0].value; //?
   let prevCol = 0;
   let row = 1;
   let col = 0;
   associativePyramid.length //?
 
+  // loop to find highest value path
   while (row < associativePyramid.length) {
     
     row; //?
     const originalCol = associativePyramid[row][col].col //?
     const colValue = associativePyramid[row][col].value  //?
     
-    // the difference of the original column value and the current column equal positioning in the sorted row
+    // the difference of the original column value and the current column equals positioning in the sorted row
     // if the difference is 0, same column as the previous row
-    // if the difference is 1, next column over
-    // if the difference is 2, path dead end and need to go back to the next column
+    // if the difference is 1, next column
+    // if the difference is 2, path dead end, go to next column
     const compareColumns =  originalCol - prevCol //?
     
     switch (compareColumns) {
@@ -41,9 +42,9 @@ const highestValuePath = (pyramid) => {
       
       // next column
       case 1:
+        sum = sum + colValue; //?
         prevCol = col //?
         col = col + 1 //?
-        sum = sum + colValue; //?
         row = row + 1 //?
         break;
       
@@ -71,7 +72,7 @@ const highestValuePathCopilot = (pyramid) => {
 }
 
 // highestValuePath([[3],[6, 7]]); //?
-// highestValuePath([[3],[7, 4],[4, 3, 5]]); //?
+// highestValuePath([[3],[3, 4],[4, 3, 5]]); //?
 highestValuePath([[3],[7, 4],[2, 4, 6],[8, 5, 9, 3]]); //?
 
 module.exports = highestValuePath;
