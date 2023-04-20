6eb043918debe02211c1fed0be05460d0787fd0a
Unfamous Dyson <dougdyson@gmail.com>
2023-04-20 14:12:24 -0700

    restore original comment

--- challenges/addingBigNumbers/add.js
+++ challenges/addingBigNumbers/add.js
@@ -3,7 +3,7 @@ function add(a, b) {
   // convert each element to a number and add both inputs via the index of the element
   // check for a carry and add it to the next element
   // convert the end result to a string, and return it
-  return (BigInt(a) + BigInt(b)).toString();
+  return (BigInt(a) + BigInt(b)).toString(); // fix this!
 }
 
 module.exports = add
