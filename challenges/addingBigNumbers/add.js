35863fe1641990d0d56cb0c6662d76d0bef9cafe
Unfamous Dyson <dougdyson@gmail.com>
2023-04-20 15:18:06 -0700

    pass first 2 tests

--- challenges/addingBigNumbers/add.js
+++ challenges/addingBigNumbers/add.js
@@ -1,9 +1,33 @@
 function add(a, b) {
-  // reduceRight the smallest number (as a string or as an array of numbers?)
-  // convert each element to a number and add both inputs via the index of the element
-  // check for a carry and add it to the next element
-  // convert the end result to a string, and return it
-  return (BigInt(a) + BigInt(b)).toString(); // fix this!
+
+  const finalSum = [];
+  
+  const addBigInts = (smallerNum, largerNum) => {
+    smallerNum = smallerNum.split('');
+    largerNum = largerNum.split('');
+    
+    const sum = smallerNum.reduceRight((carry, smallNum, i) => {
+      (Number(smallNum) + Number(largerNum[i]) > 9) ? carry = 1 : carry = 0;
+      // convert each element to a number and add both inputs via the index of the element
+      (carry) ? largerNum[i] = Number(largerNum[i]) + carry : largerNum[i] = Number(largerNum[i])
+      const acc = Number(smallNum) + Number(largerNum[i]);
+      console.log('smallNum:', smallNum, ', largerNum[i]:', largerNum[i] ,', acc:', acc);
+      // calculate carry
+      
+      finalSum.unshift(acc);
+      
+    }, 0);
+    console.log('finalSum:', finalSum.join(''));
+    return finalSum.join('');
+  }
+  
+  // determine which number is smaller
+  if (a.length < b.length) {
+    return addBigInts(a, b);
+  } else {
+    return addBigInts(b, a);
+  }
+
 }
 
 module.exports = add
