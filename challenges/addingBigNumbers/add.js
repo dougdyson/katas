b6bb8b3bebc7a304094702b4da8349afc2efd6e2
Unfamous Dyson <dougdyson@gmail.com>
2023-04-20 02:25:32 -0700

    add strategy

--- challenges/addingBigNumbers/add.js
+++ challenges/addingBigNumbers/add.js
@@ -1,4 +1,7 @@
 function add(a, b) {
+  // reduceRight the smallest number (as a string or as an array of numbers?)
+  // convert each element to a number and add both inputs
+  // convert the result to a string
   return (BigInt(a) + BigInt(b)).toString();
 }
 
